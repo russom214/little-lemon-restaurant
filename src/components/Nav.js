1b278ee3e1ff4ed47bbdee00b83035affc86cd0c
russom214 <russom214@gmail.com>
2023-10-20 14:49:55 -0400

    added functionality and responsive desighn for navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,22 +1,28 @@
-import React from 'react'
+import React, { useState } from "react";
 import Logo from "../images/Logo .svg";
 
 const Nav = () => {
+  const [menuOpen, setmenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setmenuOpen(!menuOpen);
+  };
+
   return (
-    <nav>
-      <a href="/">
+    <nav className={`navbar ${menuOpen ? "open" : ""}`}>
+      <a href="/" className="logo">
         <img src={Logo} alt="Logo" />
       </a>
 
       {/* Hamburger bar for Mobile Device  */}
-      <div>
+      <div className="menu-icon" onClick={toggleMenu}>
         <div className="bar"></div>
         <div className="bar"></div>
         <div className="bar"></div>
       </div>
 
       {/* Navigation items */}
-      <ul>
+      <ul className={`nav-links ${menuOpen ? "visible" : ""}`}>
         <li>
           <a href="/">Home</a>
         </li>
